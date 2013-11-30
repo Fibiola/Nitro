b1c3077f18f481a1cf95e5cca468a377ffb8b123
Fibiola <natasha.pristovsek@gmail.com>
2013-12-01 00:22:39 +0100

    created owner.rb model

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Nitro::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root to: 'visitors#new'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  # Example of how you can make it use another file other than index.html as root
+  # root to: redirect('/about.html')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
