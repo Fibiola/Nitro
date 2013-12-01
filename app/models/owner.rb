53601a6b24325d09c2085e5158aee9c868c8d427
Jonah Ruiz <jonah@pixelhipsters.com>
2013-12-01 00:03:36 -0400

    Refactor the Owner class for reusability

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,26 +1,37 @@
 class Owner
+	# This creates a birthdate method which will output the date used to initialize the class
+	attr_reader :birthdate
 
-	def name 
-		name = "a penguin"
-	end
-
-	def birthdate
+	def initialize(year, month, day)
 		# This is taken from Ruby API - Date.new  method gets clock from the computer 
 		# The numbers are parameters - year, month and day
-		birthdate = Date.new(1990, 12, 22)
+		# This stores the date into the birthdate instance method so you can call it like a method
+		@birthdate = Date.new(year, month, day)
+	end
+
+	def name
+		'a penguin'
+	end
+
+	def today
+		# We set today variable with today's date. It creates object that represents todays date from computer.
+		Date.today
+	end
+
+	def birthday
+		# This gives us the birthday this year
+		Date.new(today.year, birthdate.month, birthdate.day)
 	end
 
 	def countdown
-		# We set today variable with today's date. It creates object that represents todays date from computer.
-		today = Date.today
-		# We create new variable that combines todays year with brithday dates
-		# This gives us his birthday this year
-		birthday = Date.new(today.year, birthdate.month, birthdate.day)
-		# If birthday has already passed we apply a next year method to the birthday to calcualate the count
-		if birthday > today
-			countdown = (birthday - today).to_i
+		# If birthday has already passed we apply a next year method to the birthday to calculate the count
+		# This will take the result of the if / else conditional so you reduce duplication
+		countdown = if birthday > today
+			birthday - today
 		else
-			countdown = (birthday.next_year - today).to_i
+			birthday.next_year - today
 		end
+		# Now we convert the date into an integer only once :) 
+		countdown.to_i
 	end
 end
