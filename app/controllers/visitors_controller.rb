4aaf69f54630c88dfb8752beb0ef7aab527acd26
Fibiola <natasha.pristovsek@gmail.com>
2014-01-08 06:38:21 +0100

    added layout and foundation

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,17 +1,17 @@
 # We define class name it and tell it to inherit from AppController from Rails API
 class VisitorsController < ApplicationController
 
-  def new 
+  def new
     # @owner is an instance variable (because of @)
     # Instance variables are available in corresponding view files.
-    # Each controller action(method) responds to a request by obtaining a model (if data is needed) 
+    # Each controller action(method) responds to a request by obtaining a model (if data is needed)
     # and rendering a view.
 
     # This Ruby trick (pattern matching) will create an array that you can pass into the class
     #   Note: This is probably overkill but it would help if you want to calculate other user's birthdates
     #
     # Ok, so take a look at the star symbol, without it this wouldn't work.
-    # This is just like doing: 
+    # This is just like doing:
     #   year, month, day = 1990, 12, 22
     *birthdate = 1990, 12, 22
 
